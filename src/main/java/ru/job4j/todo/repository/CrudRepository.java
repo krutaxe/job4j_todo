567ab6efb40744594f2a02ed4f031db3c5432d79
krutaxe <krutaxe@mail.ru>
2022-12-07 22:18:51 +0300

    6. Лямбды и шаблон commander [#49295 #368052]

--- src/main/java/ru/job4j/todo/repository/CrudRepository.java
+++ src/main/java/ru/job4j/todo/repository/CrudRepository.java
@@ -3,6 +3,7 @@ package ru.job4j.todo.repository;
 import lombok.AllArgsConstructor;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -68,18 +69,22 @@ public class CrudRepository {
     }
 
     public <T> T tx(Function<Session, T> command) {
-        var session = sf.openSession();
-        try (session) {
-            var tx = session.beginTransaction();
-            T rsl = command.apply(session);
+        Session session = sf.openSession();
+        T rsl = null;
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+             rsl = command.apply(session);
             tx.commit();
-            return rsl;
         } catch (Exception e) {
-            var tx = session.getTransaction();
-            if (tx.isActive()) {
+            if (tx != null) {
                 tx.rollback();
+                throw e;
             }
-            throw e;
         }
+        finally {
+            session.close();
+        }
+        return rsl;
     }
 }
